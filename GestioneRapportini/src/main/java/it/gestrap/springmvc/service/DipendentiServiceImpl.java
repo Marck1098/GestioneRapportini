1b4570127ba954f44152a0ce3b6184663bd4a225
marco <marco@DESKTOP-PJ421S5.home-life.hub>
2020-06-26 10:01:06 +0200

    Creazione service mancanti

---
+++ GestioneRapportini/src/main/java/it/gestrap/springmvc/service/DipendentiServiceImpl.java
@@ -0,0 +1,38 @@
+package it.gestrap.springmvc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import it.gestrap.DAO.DipendentiDao;
+import it.gestrap.entita.Dipendenti;
+
+public class DipendentiServiceImpl implements DipendentiService {
+
+	@Autowired
+	private DipendentiDao dipendentiDAO;
+
+	@Override
+	@Transactional
+	public  List<Dipendenti> getAll() {
+		return dipendentiDAO.getAll();
+	}
+
+	@Override
+	@Transactional
+	public void save(Dipendenti theCustomer) {
+		dipendentiDAO.save(theCustomer);
+	}
+
+	@Override
+	@Transactional
+	public Dipendenti get(int theId) {
+		return dipendentiDAO.get(theId);
+	}
+
+	@Override
+	@Transactional
+	public void delete(int theId) {
+		dipendentiDAO.delete(theId);
+	}
+}
